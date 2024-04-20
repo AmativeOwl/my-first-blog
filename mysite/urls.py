84723c2a3c8fe9b286400b7824bf31e6c696dbd6
Claire <claire.wong28398@gmail.com>
2024-04-20 13:28:13 +1000

    Changed the HTML for the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    # for every url that starts with admin/
+    # django will find a corresponding "view"
     path('admin/', admin.site.urls),
+    # instructions found under blog.urls
+    path('', include('blog.urls'))
 ]
